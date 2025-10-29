ec48733830c9d3bafe5c55edc50b60c358075779
Geordie-Castaneda <jordyorantes9@gmail.com>
2025-10-29 10:30:22 -0600

    [REF]Agregar imagen

--- models/purchase_order_line.py
+++ models/purchase_order_line.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
@@ -9,5 +9,15 @@ class PurchaseOrderLine(models.Model):
         string="Imagen del Producto",
         max_width=1024,
         max_height=1024,
-        help="Imagen asociada al producto en esta línea"
+        help="Imagen asociada al producto en esta línea",
+        compute='_compute_product_image',
+        store=True
     )
+
+    @api.depends('product_id')
+    def _compute_product_image(self):
+        for record in self:
+            if record.product_id and record.product_id.image_1920:
+                record.product_image = record.product_id.image_1920
+            else:
+                record.product_image = False
